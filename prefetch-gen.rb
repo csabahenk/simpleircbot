27a9253b53739ee201f03a2262a9a43b5b86439d
Csaba Henk <csaba@lowlife.hu>
2017-01-23 12:59:30 +0000

    refactor option handling
    - move opt parsing code into SimleOpts module
    - fix issue of losing type constrainsts
      (due to leftover numeric index retrieval from a
      container that used to be an array but now is symbol
      keyed hash)
    - get rid of empty string / nil value ambiguity and
      implied "obj.to_s.empty?" condition anti-pattern
    - instead of pasting the option mangling spaghetti,
      use SimpleOpts in prefetch-gen too

--- prefetch-gen.rb
+++ prefetch-gen.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/env ruby
 
-require 'optparse'
-
 load "simpleircbot"
 
 OPTS = {
@@ -11,35 +9,9 @@ OPTS = {
   server: "<ignored>",
   channel: "<ignored>",
   nick: "<ignored>",
-}.map { |o,d| [o, [d]] }.to_h
-fixer = { Fixnum => Integer }
-OptionParser.new { |op|
-   op.banner = "Scrape bugzilla and gerrit ids from logs and dump as suitable for 'simpleircbot --cache-prefetch'"
-
-   OPTS.each { |o,w|
-     # Mangling OPTS to OptionParser options in a disgraced manner
-     # - opt_name: <scalar> becomes:
-     #   op.on("-o", "--opt-name", fixer[<scalar>.class], <scalar>.to_s) {...}
-     #   where fixer is needed to map arbitrary classes into the class set
-     #   accepted by OptionParser
-     # - opt_name: <class> becomes: op.on("-o", "--opt-name", <class>) {...}
-     op.on("-#{o[0]}", "--#{o.to_s.gsub "_", "-"}=VAL", *(
-       (Class === w[0] ? [] : [w[0].class]) << w[0]
-     ).instance_eval {|a|
-         [fixer[a[0]]||a[0]] + a[1..-1].map(&:to_s)
-     }) { |v| OPTS[o] << v }
-   }
-}.parse!
-OPTS.each { |o,w|
-  v = w[1] || w[0]
-  if Class === v
-    puts "missing value for --#{o}"
-    exit 1
-  end
-  OPTS[o] = v
 }
 
-bot = SimpleIrcBot.new(**OPTS)
+bot = SimpleIrcBot.new(**SimpleOpts.get(OPTS))
 
 log = $<.read
 data = {"bz"=>:_BUGZILLA_RX, "gerrit"=>:_GERRIT_RX}.map { |t,rx|
